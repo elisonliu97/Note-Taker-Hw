867ef019d16b62f7bc76e37bb9faeb767eb37e11
Elison Liu <elisonliu97@gmail.com>
2021-06-17 22:19:41 -0400

    created delete function

--- server.js
+++ server.js
@@ -11,32 +11,41 @@ const PORT = process.env.PORT || 3000;
 // EXPRESS APP DATA PARSING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // DATABASE
-const notes= JSON.parse(fs.readFileSync(path.join(__dirname, 'db/db.json')));
+let notes= JSON.parse(fs.readFileSync(path.join(__dirname, 'db/db.json')));
 
 // ROUTES
+// GET ROUTES
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(notes));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-
+// POST ROUTES
 app.post('/api/notes', (req, res) => {
     let newNote = req.body;
 
     newNote.id = uuidv4();
 
-    console.log(newNote)
+    console.log(newNote);
 
     notes.push(newNote);
     fs.writeFileSync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
-    res.json(newNote)
+    res.json(newNote);
 })
 
-
+// DELETE ROUTES
+app.delete('/api/notes/:id', (req, res) => {
+    const chosen = req.params.id;
+    notes = notes.filter((value) => {
+        return (value.id !== chosen);
+    })
+    fs.writeFileSync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+    res.send(`Deleted ${chosen}`)
+})
 
 
 
