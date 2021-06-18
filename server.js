50541a60e735b6f474b9faf063c7a6718aa8e654
Elison Liu <elisonliu97@gmail.com>
2021-06-17 21:45:25 -0400

    added post functions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 // EXPRESS APP
 const app = express();
@@ -16,14 +17,28 @@ app.use(express.static('public'))
 const notes= JSON.parse(fs.readFileSync(path.join(__dirname, 'db/db.json')));
 
 // ROUTES
-
-console.log(notes)
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.json(notes))
+app.get('/api/notes', (req, res) => res.json(notes));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+
+    newNote.id = uuidv4();
+
+    console.log(newNote)
+
+    notes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+    res.json(newNote)
+})
+
+
+
+
 
 // SERVER LISTENING
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
